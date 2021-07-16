54cc53092b5f2f1b4e11ce59cc52ff0b21f6d020
SeyedAb2 <tiggerwitht1378@gmail.com>
2021-07-16 23:53:56 +0430

    got first covid us stats requests done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,20 @@
+<script context=module>
+    import requests from "../data/request";
+    export async function preload(){
+        try{
+            const usStats = await requests.usStats();
+            return { usStats };
+        }catch(e){
+            
+        }
+    }
+</script>
 <script>
     import CovidState from "../components/CovidState.svelte";
     import CovidChart from "../components/CovidChart.svelte";
     import TableContainer from "../components/TableContainer.svelte";
+    export let usStats;
+    console.log(usStats,'usStats');
 </script>
 <svelte:head>
     <title>
